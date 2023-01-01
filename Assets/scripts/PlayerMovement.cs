b2fec66348c43661df3b9cecc2565e8eea9e4a27
rgquantum <fernandoluisflores94@gmail.com>
2023-01-01 20:42:22 +0800

    TEST

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;      
+    bool crouch = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;  
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false; 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump); //move our character
+        jump = false;    
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+         animator.SetBool("IsCrouching", isCrouching);
+    }
+}
