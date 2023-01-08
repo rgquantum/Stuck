dd7b850a9a4c8c9628f5bf873b04e779fade60fa
rgquantum <fernandoluisflores94@gmail.com>
2023-01-08 21:23:35 +0800

    sheesh

--- Assets/levelChoice.cs
+++ Assets/levelChoice.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class levelChoice : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,40 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Level1()
+    {
+        
+    }
+
+    public void Level2()
+    {
+
+    }
+
+    public void Level3()
+    {
+
+    }
+
+    public void Level4()
+    {
+
+    }
+
+    public void Level5()
+    {
+
+    }
+
+    public void Level6()
+    {
+
+    }
+
+    public void Level7()
+    {
+
     }
 }
--- Assets/scripts/GAMEOVER.cs
+++ Assets/scripts/GAMEOVER.cs
@@ -11,7 +11,7 @@ public class GAMEOVER : MonoBehaviour
 
     void Start()
     {
-        lastScene = SceneManager.GetActiveScene().buildIndex + 1;
+        lastScene = SceneManager.GetActiveScene().buildIndex - 1;
     }
 
     // Update is called once per frame
